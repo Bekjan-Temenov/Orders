61b2a404f837be7a63c2d6fed1ea96edb3eb4158
Toshmotov <toshmatov.shr@gmail.com>
2025-01-14 17:12:11 +0300

    проект завершен

--- server.js
+++ server.js
@@ -9,11 +9,16 @@ import { swaggerUi, swaggerDocs } from "./swagger.js";
 
 const { Pool } = pkg;
 const app = express();
+
+const host = "65.108.146.252";
 const port = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "https://shumsale.com:3443", 
+      `http://${host}:${port}`,  
+    ],
     methods: ["GET, POST, PUT, DELETE"],
     allowedHeaders: ["Content-Type"],
   })
@@ -21,10 +26,11 @@ app.use(
 
 export const pool = new Pool({
   user: "postgres",
-  password: "bekjan000",
-  host: "localhost",
+  password: "Qwerty1234",
+  host: host, 
   port: 5432,
-  database: "waildberiez",
+  database: "orders",
+  client_encoding: "UTF8",
 });
 
 pool.connect((err, client, release) => {
@@ -47,7 +53,7 @@ app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 export default app;
 
 if (process.env.NODE_ENV !== "test") {
-  app.listen(port, () => {
-    console.log(`Сервер запущен на http://localhost:${port}/api`);
+  app.listen(port, host, () => {
+    console.log(`Сервер запущен на http://${host}:${port}/api`);
   });
 }
